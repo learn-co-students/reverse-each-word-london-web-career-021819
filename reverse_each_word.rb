27c98f8ed9586b6dae02c5cd8fa44a4db550f113
Khang Le <khang.le27@gmail.com>
2019-02-15 14:27:40 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  reversed_array = array.each {|string| string.reverse!}
+  reversed_sentence = reversed_array.join(" ")
+  return reversed_sentence
+end
+
+def reverse_each_word(sentence)
+  sentence.split(" ").collect {|string| string.reverse!}.join(" ")
+end
+
